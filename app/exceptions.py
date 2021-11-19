3c647a6192d73990e1740423b152f7829956b2f2
enchance <enchance@gmail.com>
2021-11-19 22:18:40 +0800

    Update to exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,15 +1,19 @@
 from fastapi import HTTPException
 from typing import Optional, Any, Dict
 
+from app import settings as s
 
-GENERIC_422 = 422
-PERMISSION_DENIED_403 = 403
-SERVICE_UNAVAILABLE_503 = 503
+
+
+GENERIC_400 = 400               # If you don't know what to send
+UNPROCESSABLE_422 = 422     # I understand it but am not allowing it (e.g. invalid values, etc.)
+PERMISSION_DENIED_403 = 403     # You can't do that it's bad and you should feel bad
+SERVICE_UNAVAILABLE_503 = 503   # Your db exploded, server burned down, etc.
 
 
 class BaseAppError(HTTPException):
-    message = 'NO MESSAGE INCLUDED'
-    status_code = GENERIC_422
+    message = 'UNABLE TO CONTINUE'
+    status_code = GENERIC_400
 
     def __init__(self, detail: Optional[Any] = None, status_code: Optional[int] = None,
                  *, headers: Optional[Dict[str, Any]] = None) -> None:
@@ -18,15 +22,20 @@ class BaseAppError(HTTPException):
         super().__init__(status_code=status_code, detail=detail, headers=headers)
 
 
+class GenericError(BaseAppError):
+    """I don't understand what you're trying to do."""
+    message = "I DON'T UNDERSTAND WHAT YOU WANT ME TO DO"
+    
+    
 class FalsyDataError(BaseAppError):
     """Data is falsy such as '', [], None, {}, set(), False, etc.."""
-    message = "FALSY DATA OR NONE"
-    status_code = GENERIC_422
+    message = "FALSY OR None data supplied"
+    status_code = UNPROCESSABLE_422
 
 
 class ServiceError(BaseAppError):
-    """Unable to continue work because of a service error (e.g. database)."""
-    message = 'UNABLE TO PROCESS DATA'
+    """Unable to continue because a service is down (e.g. database)."""
+    message = 'SERVICE UNAVAILABLE'
     status_code = SERVICE_UNAVAILABLE_503
 
 
@@ -39,7 +48,7 @@ class PermissionDenied(BaseAppError):
 class NotFoundError(BaseAppError):
     message = "DATA NOT FOUND"
     # Not 404 since pov is from the client (422) not the app (404)
-    status_code = GENERIC_422
+    status_code = GENERIC_400
     
     def __init__(self, model: str = None):
         message = s.DEBUG and model and f'{model} not found' or self.message
