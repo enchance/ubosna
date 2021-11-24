2746faa35c81c9fe2c7818f1f7de3c172f842aa9
enchance <enchance@gmail.com>
2021-11-24 17:30:04 +0800

    Create utils file

---
+++ app/utils.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def flatten_query_result(key: str, query_result: List[dict], unique: bool = True) -> list:
+    """
+    Used for list of dict taken from db results. Get only the values of the key supplied.
+    :param key:             Dict key ot extract.
+    :param query_result:    List of values from the key specified
+    :param unique:          Only return unique values
+    :return:                list
+    """
+    flattened = [i[key] for i in query_result]
+    if unique:
+        return list(set(flattened))
+    return flattened
